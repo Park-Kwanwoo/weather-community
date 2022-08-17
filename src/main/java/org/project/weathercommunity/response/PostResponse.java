281f19eb32be7a56a40b39f6ca983e2c0c56d60b
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-18 00:15:08 +0900

    생성자 오버로딩

--- src/main/java/org/project/weathercommunity/response/PostResponse.java
+++ src/main/java/org/project/weathercommunity/response/PostResponse.java
@@ -2,6 +2,7 @@ package org.project.weathercommunity.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.project.weathercommunity.domain.Post;
 
 /**
  * 서비스 정책에 맞는 응답 클래스
@@ -13,6 +14,12 @@ public class PostResponse {
     private final String title;
     private final String content;
 
+    // 생성자 오버로딩
+    public PostResponse(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+    }
     @Builder
     public PostResponse(Long id, String title, String content) {
         this.id = id;
