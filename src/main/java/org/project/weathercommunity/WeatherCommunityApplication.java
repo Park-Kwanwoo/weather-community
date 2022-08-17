d8e9cd978624026ae85f362b121caffb6baadf74
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-18 00:16:03 +0900

    first commit

--- src/main/java/org/project/weathercommunity/WeatherCommunityApplication.java
+++ src/main/java/org/project/weathercommunity/WeatherCommunityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherCommunityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WeatherCommunityApplication.class, args);
     }
