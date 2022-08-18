07b1e31d0ebf459adaa6a59f0d2611f09d491d69
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-18 22:42:24 +0900

    postEdit domain

---
+++ src/main/java/org/project/weathercommunity/request/PostEdit.java
@@ -0,0 +1,28 @@
+package org.project.weathercommunity.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.project.weathercommunity.domain.Post;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@ToString
+public class PostEdit {
+
+    @NotBlank(message = "제목을 입력하세요")
+    private String title;
+
+    @NotBlank(message = "내용을 입력하세요.")
+    private String content;
+
+    @Builder
+    public PostEdit(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
