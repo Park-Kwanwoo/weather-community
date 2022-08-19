ff3ae4052720241e716063129cc68c2dd6eb8956
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-19 18:17:56 +0900

    import 정리

--- src/main/java/org/project/weathercommunity/domain/Post.java
+++ src/main/java/org/project/weathercommunity/domain/Post.java
@@ -4,7 +4,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.project.weathercommunity.request.PostEdit;
 
 import javax.persistence.*;
 
