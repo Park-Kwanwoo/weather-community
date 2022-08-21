6aab0ce2671cf3b32704e1ab6ba3a25d24486ff0
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-21 18:45:20 +0900

    Content, Header vue 추가

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import Sidebar from "@/components/Sidebar.vue";
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/write">글 작성</RouterLink>
-      </nav>
-  </header>
-  <RouterView />
+  <Header />
+  <Sidebar />
 </template>
 
 <style>
+body {
+  margin: 0;
+  width: 100%;
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+  "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo",
+  "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+  sans-serif;
+}
 </style>
---
+++ front/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-header class="header">
+    <el-menu mode="horizontal">
+
+    </el-menu>
+  </el-header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style >
+
+</style>
