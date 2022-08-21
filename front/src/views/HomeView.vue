744234f3d397d2d6d54952cb3d7372f968821949
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-21 18:45:40 +0900

    css 추가

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -12,21 +12,46 @@ axios.get("/api/posts?page=1&size=10").then((response) => {
   })
 })
 
-const moveToRead = () => {
-  router.push({name : "read"});
-}
-</script>
+가</script>
 
 <template>
   <ul>
-    <li v-for="post in posts" :key="post.id" @click="moveToRead()">
-      <div>
+    <li v-for="post in posts" :key="post.id">
+      <div class="title">
         <router-link :to="{name: 'read', params: { postId: post.id}}">{{post.title}}</router-link>
       </div>
 
-      <div>
+      <div class="content">
         {{post.content}}
       </div>
     </li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  margin-bottom: 1.3rem;
+}
+
+li .title {
+  font-size: 1.2rem;
+  color: #303030;
+}
+
+li .title > a {
+  text-decoration: none;
+}
+
+li .content {
+  font-size: 0.95rem;
+  color: #5d5d5d;
+}
+
+li:last-child {
+  margin-bottom: 0;
+}
+</style>
