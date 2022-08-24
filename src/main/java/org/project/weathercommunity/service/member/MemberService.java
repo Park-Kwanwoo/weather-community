45521f1c183c20ded9cc67b2a08406d00de20498
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-24 22:19:07 +0900

    멤버 수정 기능 추가

--- src/main/java/org/project/weathercommunity/service/member/MemberService.java
+++ src/main/java/org/project/weathercommunity/service/member/MemberService.java
@@ -1,14 +1,19 @@
 package org.project.weathercommunity.service.member;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.project.weathercommunity.domain.member.Member;
-import org.project.weathercommunity.domain.member.Role;
+import org.project.weathercommunity.domain.member.MemberEditor;
+import org.project.weathercommunity.exception.MemberNotFound;
 import org.project.weathercommunity.repository.member.MemberRepository;
 import org.project.weathercommunity.request.member.MemberCreate;
+import org.project.weathercommunity.request.member.MemberEdit;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,5 +28,31 @@ public class MemberService {
 
         memberRepository.save(member);
 
+    }
+
+    @Transactional
+    public void edit(Long id, MemberEdit memberEdit) {
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(MemberNotFound::new);
+
+        MemberEditor.MemberEditorBuilder editorBuilder = member.toEditor();
+
+        MemberEditor memberEditor = editorBuilder
+                .name(memberEdit.getName())
+                .password(memberEdit.getPassword())
+                .phone(memberEdit.getPhone())
+                .build();
+
+
+        member.edit(memberEditor);
+    }
+
+    public void delete(Long id) {
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(MemberNotFound::new);
+
+        memberRepository.delete(member);
     }
 }
