adabcf868610ade3773bce6bd54e0c3c362fba7e
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-24 22:18:21 +0900

    validation 패턴 추가

--- src/main/java/org/project/weathercommunity/request/member/MemberCreate.java
+++ src/main/java/org/project/weathercommunity/request/member/MemberCreate.java
@@ -7,20 +7,25 @@ import org.project.weathercommunity.domain.member.Role;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class MemberCreate {
 
     @NotBlank(message = "이메일을 입력해주세요.")
-    @Email(message = "제대로된 이메일 형식으로 입력해주세요.")
+
+    @Email(message = "제대로된 이메일 형식으로 입력해주세요.", regexp = "^[\\w\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
     private String email;
     @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
+             message = "숫자, 문자, 특수문자 포함 8~15자리 이내로 입력해주세요.")
     private String password;
     @NotBlank(message = "이름을 입력해주세요.")
     private String name;
 
     @NotBlank(message = "전화번호를 입력해주세요.")
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식에 맞게 입력해주세요.")
     private String phone;
 
     private Role role;
