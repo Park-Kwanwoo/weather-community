66705f46865b074f495353435174f990c227edc8
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-31 22:33:56 +0900

    회원 찾기

--- src/main/java/org/project/weathercommunity/repository/member/MemberRepository.java
+++ src/main/java/org/project/weathercommunity/repository/member/MemberRepository.java
@@ -3,7 +3,9 @@ package org.project.weathercommunity.repository.member;
 import org.project.weathercommunity.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
+    Optional<Member> findByEmail(String email);
 }
