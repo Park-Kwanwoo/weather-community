016546fb73c31664cfa2dcf611c80b72bc0895c0
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-02 21:14:20 +0900

    Security 기본 설정

--- src/main/java/org/project/weathercommunity/config/security/SecurityConfig.java
+++ src/main/java/org/project/weathercommunity/config/security/SecurityConfig.java
@@ -1,18 +1,11 @@
 package org.project.weathercommunity.config.security;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,7 +18,7 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
@@ -37,7 +30,16 @@ public class SecurityConfig {
                 .antMatchers("/members/join", "/members/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("http://localhost:5174/login")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .loginProcessingUrl("/login_proc")
+                .successHandler(((request, response, authentication) -> response.sendRedirect("http://localhost:5174")))
+                .failureUrl("http://localhost:5174")
+                .failureHandler((request, response, e) -> {
+                    response.sendRedirect("http://localhost:5174/login?e=" + e.getMessage());
+                });
 
 
         return http.build();
