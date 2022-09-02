05b2f9e47135830cf3427661ebc8658e5aabe29a
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-02 21:13:20 +0900

    로그인 화면

--- front/src/views/user/LoginView.vue
+++ front/src/views/user/LoginView.vue
@@ -1,34 +1,47 @@
 <template>
-  <div>
-    <el-input v-model="email" placeholder="아이디"/>
-  </div>
-
-  <div class="mt-2">
-    <el-input v-model="password" type="password" rows="15"></el-input>
-  </div>
-
-  <div class="mt-2">
-    <el-button type="primary" @click="login()">로그인</el-button>
-  </div>
-
+  <el-form
+      :model="loginForm"
+      label-width="120px"
+      class="demo-ruleForm">
+    <el-form-item
+        label="email">
+      <el-input name="email" v-model="loginForm.email" type="text"/>
+    </el-form-item>
+      <el-form-item label="password">
+        <el-input name="password" v-model="loginForm.password" type="password"/>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm">로그인</el-button>
+    </el-form-item>
+  </el-form>
+<!--  <form method="post" action="http://localhost:8080/login_proc">-->
+<!--    <input type="text" title="email" name="email" placeholder="username" />-->
+<!--    <input type="password" name="password" placeholder="password" />-->
+<!--    <button type="submit" class="btn">Login</button>-->
+<!--  </form>-->
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import { reactive, ref } from 'vue'
 import axios from "axios";
 
-const email = ref("");
-const password = ref("")
+const loginForm = reactive({
+  email: '',
+  password: '',
+})
 
-const login = function () {
-  axios.post("/api/members/join", {
-    email: email.value,
-    password: password.value
-  }).then((res) => {
-    console.log(res)
-  }).catch((error) => {
-    console.log(error);
-    throw new Error();
-  })
+const configs = {
+  headers: {
+    "contentType": "x-www-form-urlencoded"
+  }
+}
+
+const submitForm = () => {
+  axios.postForm("/api/login_proc", loginForm, configs)
+      .then((res) => {
+        console.log(res)
+      }).catch((error) => {
+        console.log(error);
+      })
 }
 </script>
