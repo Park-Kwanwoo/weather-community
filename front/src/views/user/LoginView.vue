cff33ac6f67887c8193cd86ea40deb1144be2948
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-04 20:35:35 +0900

    Vue 로그인 기능

--- front/src/views/user/LoginView.vue
+++ front/src/views/user/LoginView.vue
@@ -1,47 +1,47 @@
 <template>
   <el-form
-      :model="loginForm"
+      v-on:click.prevent="submitForm"
       label-width="120px"
       class="demo-ruleForm">
     <el-form-item
         label="email">
-      <el-input name="email" v-model="loginForm.email" type="text"/>
+      <el-input name="email" v-model="loginData.email" type="text"/>
     </el-form-item>
       <el-form-item label="password">
-        <el-input name="password" v-model="loginForm.password" type="password"/>
+        <el-input name="password" v-model="loginData.password" type="password"/>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm">로그인</el-button>
+      <el-button type="submit" class="btn-primary">로그인</el-button>
     </el-form-item>
   </el-form>
-<!--  <form method="post" action="http://localhost:8080/login_proc">-->
-<!--    <input type="text" title="email" name="email" placeholder="username" />-->
-<!--    <input type="password" name="password" placeholder="password" />-->
-<!--    <button type="submit" class="btn">Login</button>-->
-<!--  </form>-->
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
 import axios from "axios";
+import {reactive} from "vue";
+import {useRouter} from "vue-router";
 
-const loginForm = reactive({
+const router = useRouter();
+
+const loginData = reactive({
   email: '',
   password: '',
 })
 
 const configs = {
   headers: {
-    "contentType": "x-www-form-urlencoded"
+    "X-Requested-With": "XMLHttpRequest"
   }
 }
 
 const submitForm = () => {
-  axios.postForm("/api/login_proc", loginForm, configs)
-      .then((res) => {
-        console.log(res)
-      }).catch((error) => {
-        console.log(error);
+  axios.post("/api/members/login", loginData, configs)
+      .then((r) => {
+        console.log(r)
+      })
+      .catch(e => {
+        console.log(e)
+        router.replace("/login?error=" + e.response.data);
       })
 }
 </script>
