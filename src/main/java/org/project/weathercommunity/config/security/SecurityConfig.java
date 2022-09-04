d253ec8f0fada080f9e0390204745b873ae57122
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-04 20:35:00 +0900

    SecurityConfig 설정

--- src/main/java/org/project/weathercommunity/config/security/SecurityConfig.java
+++ src/main/java/org/project/weathercommunity/config/security/SecurityConfig.java
@@ -1,47 +1,81 @@
 package org.project.weathercommunity.config.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.project.weathercommunity.config.security.filter.VueLoginProcessingFilter;
+import org.project.weathercommunity.config.security.handler.VueAuthenticationFailureHandler;
+import org.project.weathercommunity.config.security.handler.VueAuthenticationSuccessHandler;
+import org.project.weathercommunity.config.security.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
     @Bean
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/members/join", "/members/login").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("http://localhost:5174/login")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .loginProcessingUrl("/login_proc")
-                .successHandler(((request, response, authentication) -> response.sendRedirect("http://localhost:5174")))
-                .failureUrl("http://localhost:5174")
-                .failureHandler((request, response, e) -> {
-                    response.sendRedirect("http://localhost:5174/login?e=" + e.getMessage());
-                });
+        .and()
 
+                .addFilterBefore(new VueLoginProcessingFilter(authenticationManagerBean(), customAuthenticationSuccessHandler(), customAuthenticationFailureHandler()), UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
     }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new VueAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler customAuthenticationFailureHandler() {
+        return new VueAuthenticationFailureHandler();
+    }
+
+
+    //    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+//
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/members/join", "/members/login").permitAll()
+//                .anyRequest().authenticated()
+//        .and()
+//                .addFilterBefore(new VueLoginProcessingFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
 }
