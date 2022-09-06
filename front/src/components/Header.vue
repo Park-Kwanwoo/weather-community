dad1290e97d8868589a963425d7390d66762880a
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-06 19:03:30 +0900

    인증 후 Header item 변경

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -2,13 +2,21 @@
   <el-header class="header">
     <el-menu mode="horizontal">
       <img alt="" src="">
-      <el-menu-item>Item 1</el-menu-item>
-      <el-menu-item>Item 2</el-menu-item>
+      <el-menu-item><router-link v-if="!isAuth" to="login">Login</router-link></el-menu-item>
+      <el-menu-item><router-link v-if="!isAuth" to="join">Join</router-link></el-menu-item>
+      <el-menu-item><router-link v-if="isAuth" to="join">내 정보</router-link></el-menu-item>
     </el-menu>
   </el-header>
 </template>
 
 <script setup lang="ts">
+
+import {useAuthStore} from "@/stores/auth";
+import {storeToRefs} from "pinia";
+
+const auth = useAuthStore();
+const { isAuth } = storeToRefs(auth)
+
 
 </script>
 
