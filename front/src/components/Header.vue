9ff5c8c527bd44b1d43f3cbd929b2325edd4479a
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-06 22:13:02 +0900

    페이지 이동 함수 추가

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -2,9 +2,10 @@
   <el-header class="header">
     <el-menu mode="horizontal">
       <img alt="" src="">
-      <el-menu-item><router-link v-if="!isAuth" to="login">Login</router-link></el-menu-item>
-      <el-menu-item><router-link v-if="!isAuth" to="join">Join</router-link></el-menu-item>
-      <el-menu-item><router-link v-if="isAuth" to="join">내 정보</router-link></el-menu-item>
+      <el-menu-item v-if="!getIsAuth" @click="login">로그인</el-menu-item>
+      <el-menu-item v-if="!getIsAuth" @click="join">회원가입</el-menu-item>
+      <el-menu-item v-if="getIsAuth" @click="myInfo">내 정보</el-menu-item>
+      <el-menu-item v-if="getIsAuth" @click="logout">로그아웃</el-menu-item>
     </el-menu>
   </el-header>
 </template>
@@ -13,11 +14,25 @@
 
 import {useAuthStore} from "@/stores/auth";
 import {storeToRefs} from "pinia";
+import {useRouter} from "vue-router";
 
 const auth = useAuthStore();
-const { isAuth } = storeToRefs(auth)
+const { getIsAuth } = storeToRefs(auth)
+const router = useRouter();
 
-
+const logout = function () {
+  auth.clear();
+  router.replace({name: 'home'})
+};
+const login = () => {
+  router.replace( {name: 'login'})
+}
+const join = () => {
+  router.replace( {name: 'join'})
+}
+const myInfo = () => {
+  router.replace( {name: 'myInfo'})
+}
 </script>
 
 <style >
