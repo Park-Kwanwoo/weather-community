3a2032128fd346e2b1b5565a9ac52e316aba1ac5
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-06 19:01:20 +0900

    로그인 응답 dto

--- src/main/java/org/project/weathercommunity/request/member/MemberLogin.java
+++ src/main/java/org/project/weathercommunity/request/member/MemberLogin.java
@@ -2,13 +2,14 @@ package org.project.weathercommunity.request.member;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
 public class MemberLogin {
 
     @NotBlank(message = "아이디를 입력해주세요")
@@ -16,4 +17,10 @@ public class MemberLogin {
 
     @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
+
+    @Builder
+    public MemberLogin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
