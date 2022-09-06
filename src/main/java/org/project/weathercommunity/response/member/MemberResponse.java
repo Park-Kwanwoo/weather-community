b3859c109829539b4415a4ce56cd05d9ff0e11d2
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-06 19:01:05 +0900

    로그인 응답 dto

--- src/main/java/org/project/weathercommunity/response/member/MemberResponse.java
+++ src/main/java/org/project/weathercommunity/response/member/MemberResponse.java
@@ -3,27 +3,22 @@ package org.project.weathercommunity.response.member;
 import lombok.Builder;
 import lombok.Getter;
 import org.project.weathercommunity.domain.member.Member;
+import org.project.weathercommunity.domain.member.Role;
 
 @Getter
 public class MemberResponse {
 
     private final String email;
-    private final String password;
-    private final String name;
-    private final String phone;
+    private final Role role;
 
     public MemberResponse(Member member) {
         this.email = member.getEmail();
-        this.password = member.getPassword();
-        this.name = member.getName();
-        this.phone = member.getPhone();
+        this.role = member.getRole();
     }
 
     @Builder
-    public MemberResponse(String email, String password, String name, String phone) {
+    public MemberResponse(String email, Role role) {
         this.email = email;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
+        this.role = role;
     }
 }
