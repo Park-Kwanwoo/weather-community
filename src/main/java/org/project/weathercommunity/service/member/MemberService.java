ae2ccaacbd9696191b6391885076a0225325c059
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-07 22:06:14 +0900

    회원 단건 조회 추가

--- src/main/java/org/project/weathercommunity/service/member/MemberService.java
+++ src/main/java/org/project/weathercommunity/service/member/MemberService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.project.weathercommunity.domain.member.Member;
 import org.project.weathercommunity.domain.member.MemberEditor;
+import org.project.weathercommunity.exception.InvalidRequest;
 import org.project.weathercommunity.exception.MemberNotFound;
 import org.project.weathercommunity.repository.member.MemberRepository;
 import org.project.weathercommunity.request.member.MemberCreate;
 import org.project.weathercommunity.request.member.MemberEdit;
+import org.project.weathercommunity.response.member.MemberMypageResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,7 +48,7 @@ public class MemberService {
 
         MemberEditor memberEditor = editorBuilder
                 .name(memberEdit.getName())
-                .password(memberEdit.getPassword())
+//                .password(memberEdit.getPassword())
                 .phone(memberEdit.getPhone())
                 .build();
 
@@ -59,6 +62,26 @@ public class MemberService {
                 .orElseThrow(MemberNotFound::new);
 
         memberRepository.delete(member);
+    }
+
+    public MemberMypageResponse get(Long id, Authentication authentication) {
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(MemberNotFound::new);
+
+        Member currentMember = (Member) authentication.getPrincipal();
+
+        if (member.getId().equals(currentMember.getId())) {
+            return MemberMypageResponse.builder()
+                    .id(member.getId())
+                    .email(member.getEmail())
+                    .password(member.getPassword())
+                    .name(member.getName())
+                    .phone(member.getPhone())
+                    .build();
+        } else {
+            throw  new InvalidRequest("auth", "잘못된 접근입니다.");
+        }
     }
 
     public boolean duplicateCheck(String email) {
