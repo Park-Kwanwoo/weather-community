6bc207a7da73c1d2cd81575e071bad90745adc26
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-07 22:05:14 +0900

    사이드바 정리

--- front/src/components/Sidebar.vue
+++ front/src/components/Sidebar.vue
@@ -2,30 +2,13 @@
   <el-menu
       default-active="2"
       class="el-menu-vertical-demo">
-    <el-sub-menu index="1">
-      <template #title>
-        <el-icon><location /></el-icon>
-        <span>날씨 보기</span>
-      </template>
-        <el-menu-item index="1-3">
-          <el-link><router-link to="#">지역별 날씨</router-link></el-link>
-        </el-menu-item>
-<!--      <el-sub-menu index="1-4">-->
-<!--        <template #title><span>item four</span></template>-->
-<!--        <el-menu-item index="1-4-1">item one</el-menu-item>-->
-<!--      </el-sub-menu>-->
-    </el-sub-menu>
-    <el-menu-item index="2">
+    <el-menu-item index="1">
       <el-icon><icon-menu /></el-icon>
-      <el-link><router-link to="/post/write">글 쓰러가기</router-link></el-link>
+      <el-link @click="write">글 쓰러가기</el-link>
     </el-menu-item>
-    <el-menu-item index="3">
+    <el-menu-item index="2">
       <el-icon><document /></el-icon>
       <template #title><router-link to="/posts">글 목록</router-link></template>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <el-icon><setting /></el-icon>
-      <template #title>Navigator Four</template>
     </el-menu-item>
   </el-menu>
 </template>
@@ -37,6 +20,24 @@ import {
   Location,
   Setting,
 } from '@element-plus/icons-vue'
+import {useAuthStore} from "@/stores/auth";
+import {storeToRefs} from "pinia";
+import {useRouter} from "vue-router";
+
+
+const auth = useAuthStore();
+const { getIsAuth } = storeToRefs(auth)
+const router = useRouter();
+
+const write = function () {
+
+  if (getIsAuth.value) {
+    console.log(getIsAuth.value);
+    router.push({name: 'write'})
+  } else {
+    router.replace({name: 'login'})
+  }
+};
 
 </script>
 
