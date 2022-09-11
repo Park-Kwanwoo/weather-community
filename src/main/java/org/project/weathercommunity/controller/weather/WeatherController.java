e9d33e55e209f0ce59734d596aa17842bc7f59af
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-11 22:18:06 +0900

    날씨 데이터 Controller, Service

--- src/main/java/org/project/weathercommunity/controller/weather/WeatherController.java
+++ src/main/java/org/project/weathercommunity/controller/weather/WeatherController.java
@@ -2,10 +2,15 @@ package org.project.weathercommunity.controller.weather;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.project.weathercommunity.request.weather.WeatherInfo;
 import org.project.weathercommunity.service.weather.WeatherService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,9 +19,8 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
-
-    @GetMapping("/weather")
-    public Mono<String> weatherInformation() {
-        return weatherService.getDayWeather();
+    @PostMapping("/weather")
+    public Mono<String> weatherInformation(@RequestBody @Valid WeatherInfo weatherInfo) {
+        return weatherService.getDayWeather(weatherInfo);
     }
 }
--- src/main/java/org/project/weathercommunity/service/weather/WeatherService.java
+++ src/main/java/org/project/weathercommunity/service/weather/WeatherService.java
@@ -1,11 +1,15 @@
 package org.project.weathercommunity.service.weather;
 
+import org.project.weathercommunity.common.GpsTransfer;
+import org.project.weathercommunity.request.weather.WeatherInfo;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Service
 public class WeatherService {
@@ -16,7 +20,7 @@ public class WeatherService {
     private static final String PAGE_NO = "1";
     private static final String DATA_TYPE = "JSON";
 
-    public Mono<String> getDayWeather() {
+    public Mono<String> getDayWeather(WeatherInfo weatherInfo) {
 
         DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
         factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
@@ -27,6 +31,8 @@ public class WeatherService {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
 
+        Map<String, Integer> transfer = GpsTransfer.transfer(Double.parseDouble(weatherInfo.getNx()), Double.parseDouble(weatherInfo.getNy()));
+
         return webClient.get()
                 .uri(uriBuilder ->
                         uriBuilder.path("/getUltraSrtNcst")
@@ -34,10 +40,10 @@ public class WeatherService {
                                 .queryParam("numOfRows", NUM_OF_ROWS)
                                 .queryParam("pageNo", PAGE_NO)
                                 .queryParam("dataType", DATA_TYPE)
-                                .queryParam("nx", "37")
-                                .queryParam("ny", "124")
-                                .queryParam("base_date", "20220822")
-                                .queryParam("base_time", "1500")
+                                .queryParam("nx", transfer.get("nx"))
+                                .queryParam("ny", transfer.get("ny"))
+                                .queryParam("base_date", "20220911")
+                                .queryParam("base_time", "1200")
                                 .build())
                 .retrieve()
                 .bodyToMono(String.class);
