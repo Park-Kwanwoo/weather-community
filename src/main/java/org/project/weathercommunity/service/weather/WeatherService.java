ab72a6ad782d0dbe4d38ea3cd43d976bf8286874
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-13 23:29:26 +0900

    서비스 로직 변경

--- src/main/java/org/project/weathercommunity/service/weather/WeatherService.java
+++ src/main/java/org/project/weathercommunity/service/weather/WeatherService.java
@@ -1,5 +1,8 @@
 package org.project.weathercommunity.service.weather;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.project.weathercommunity.common.GpsTransfer;
 import org.project.weathercommunity.request.weather.WeatherInfo;
 import org.springframework.http.HttpHeaders;
@@ -16,11 +19,11 @@ public class WeatherService {
 
     private static final String BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
     private static final String SERVICE_KEY = "jS6Wrc0k0cSlZlPaVsvx3HIMQcjkWzob1pr0nJTL%2F3Ko8duvWK99XPKdoG4pMddHrSnXOK9IY%2FKvyHOo48Csjg%3D%3D";
-    private static final String NUM_OF_ROWS = "10";
+    private static final String NUM_OF_ROWS = "60";
     private static final String PAGE_NO = "1";
     private static final String DATA_TYPE = "JSON";
 
-    public Mono<String> ultraShortLiveInquiry(WeatherInfo weatherInfo) {
+    public Mono<String> ultraShortForecast(WeatherInfo weatherInfo) throws JsonProcessingException {
 
         DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
         factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
@@ -35,27 +38,17 @@ public class WeatherService {
 
         return webClient.get()
                 .uri(uriBuilder ->
-                        uriBuilder.path("/getUltraSrtNcst")
+                        uriBuilder.path("/getUltraSrtFcst")
                                 .queryParam("serviceKey", SERVICE_KEY)
                                 .queryParam("numOfRows", NUM_OF_ROWS)
                                 .queryParam("pageNo", PAGE_NO)
                                 .queryParam("dataType", DATA_TYPE)
                                 .queryParam("nx", transfer.get("nx"))
                                 .queryParam("ny", transfer.get("ny"))
-                                .queryParam("base_date", "20220912")
-                                .queryParam("base_time", "1200")
+                                .queryParam("base_date", weatherInfo.getBaseDate())
+                                .queryParam("base_time", weatherInfo.getBaseTime())
                                 .build())
                 .retrieve()
                 .bodyToMono(String.class);
-    }
-
-    public Mono<String> ultraShortForecast(WeatherInfo weatherInfo) {
-
-        return null;
-    }
-
-    public Mono<String> shortForecast(WeatherInfo weatherInfo) {
-
-        return null;
     }
 }
