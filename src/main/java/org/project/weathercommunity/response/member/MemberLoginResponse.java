38a5f4db76d912fe70bba0277b4de164a3ea3baa
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-22 17:46:00 +0900

    회원 로그인 응답 필드 변경

--- src/main/java/org/project/weathercommunity/response/member/MemberLoginResponse.java
+++ src/main/java/org/project/weathercommunity/response/member/MemberLoginResponse.java
@@ -2,27 +2,20 @@ package org.project.weathercommunity.response.member;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.project.weathercommunity.domain.member.Member;
-import org.project.weathercommunity.domain.member.Role;
 
 @Getter
 public class MemberLoginResponse {
 
     private final Long id;
     private final String email;
-    private final Role role;
 
+    private final String token;
 
-    public MemberLoginResponse(Member member) {
-        this.id = member.getId();
-        this.email = member.getEmail();
-        this.role = member.getRole();
-    }
 
     @Builder
-    public MemberLoginResponse(Long id, String email, Role role) {
+    public MemberLoginResponse(Long id, String email, String token) {
         this.id = id;
         this.email = email;
-        this.role = role;
+        this.token = token;
     }
 }
