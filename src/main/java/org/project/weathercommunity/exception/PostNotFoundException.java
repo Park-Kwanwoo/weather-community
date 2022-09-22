50e8da533ed1bad4749ceb5f1590dc690d51ffc0
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-22 17:48:29 +0900

    클래스명 변경 기존 클래스명 + Exception

--- src/main/java/org/project/weathercommunity/exception/PostNotFoundException.java
+++ src/main/java/org/project/weathercommunity/exception/PostNotFoundException.java
@@ -3,14 +3,14 @@ package org.project.weathercommunity.exception;
 /**
  * status -> 404
  */
-public class PostNotFound extends WeatherCommunityException {
+public class PostNotFoundException extends WeatherCommunityException {
 
     private static final String MESSAGE = "존재하지 않는 글입니다.";
-    public PostNotFound() {
+    public PostNotFoundException() {
         super(MESSAGE);
     }
 
-    public PostNotFound(Throwable cause) {
+    public PostNotFoundException(Throwable cause) {
         super(MESSAGE, cause);
     }
 
