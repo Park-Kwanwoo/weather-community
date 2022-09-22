1d5dfaf3d4c6527e05afa3a13b71a4bc3d7af7f0
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-22 17:43:43 +0900

    클래스명 변경

--- src/main/java/org/project/weathercommunity/exception/InvalidRequestException.java
+++ src/main/java/org/project/weathercommunity/exception/InvalidRequestException.java
@@ -3,19 +3,19 @@ package org.project.weathercommunity.exception;
 /**
  * status -> 400
  */
-public class InvalidRequest extends WeatherCommunityException {
+public class InvalidRequestException extends WeatherCommunityException {
 
     private static final String MESSAGE = "잘못된 요청입니다.";
 
-    public InvalidRequest() {
+    public InvalidRequestException() {
         super(MESSAGE);
     }
 
-    public InvalidRequest(Throwable cause) {
+    public InvalidRequestException(Throwable cause) {
         super(MESSAGE, cause);
     }
 
-    public InvalidRequest(String fieldName, String message) {
+    public InvalidRequestException(String fieldName, String message) {
         super(MESSAGE);
         addValidation(fieldName, message);
     }
