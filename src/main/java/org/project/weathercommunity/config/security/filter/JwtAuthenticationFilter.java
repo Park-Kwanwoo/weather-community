c843366b941c19397b077c8cd893cdd5585fa6aa
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-22 17:48:52 +0900

    jwt 검증 필터 로직 변경

--- src/main/java/org/project/weathercommunity/config/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/project/weathercommunity/config/security/filter/JwtAuthenticationFilter.java
@@ -2,11 +2,10 @@ package org.project.weathercommunity.config.security.filter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.project.weathercommunity.config.security.token.JwtAuthenticationToken;
+import org.project.weathercommunity.config.security.token.JwtToken;
 import org.project.weathercommunity.domain.member.Member;
 import org.project.weathercommunity.domain.token.TokenEditor;
 import org.project.weathercommunity.exception.JwtExpiredException;
-import org.project.weathercommunity.request.token.TokenRequest;
 import org.project.weathercommunity.service.token.TokenService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -25,7 +24,7 @@ import java.io.IOException;
 // 토큰 인증을 담당하는 클래스
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtAuthenticationToken jwtAuthenticationToken;
+    private final JwtToken jwtToken;
     private final TokenService tokenService;
 
     @Override
@@ -34,29 +33,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         log.info("JwtAuthenticationFilter 진입");
 
         if (SecurityContextHolder.getContext().getAuthentication() == null) {
-            String accessToken = jwtAuthenticationToken.resolveToken(request);
+            String accessToken = jwtToken.resolveToken(request);
             log.info("accessToken = {}", accessToken);
 
 
-            if (accessToken != null && jwtAuthenticationToken.validTokenExpired(accessToken)) {         // accessToken이 Null이 아니면서 만료되지 읺았을 때
+            if (accessToken != null && jwtToken.validTokenExpired(accessToken)) {         // accessToken이 Null이 아니면서 만료되지 읺았을 때
                 log.info("token이 만료 되지 않았습니다.");
-                Authentication authentication = jwtAuthenticationToken.getAuthentication(accessToken);
+                Authentication authentication = jwtToken.getAuthentication(accessToken);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-            } else if (accessToken != null && !jwtAuthenticationToken.validTokenExpired(accessToken)){  // accessToken이 Null이 아니면서 만료되었을 때
+            } else if (accessToken != null && !jwtToken.validTokenExpired(accessToken)){  // accessToken이 Null이 아니면서 만료되었을 때
 
                 log.info("token이 만료 되었습니다.");
-                Authentication authentication = jwtAuthenticationToken.getAuthentication(accessToken);
+                Authentication authentication = jwtToken.getAuthentication(accessToken);
                 Member member = (Member) authentication.getPrincipal();
 
                 String refreshToken = tokenService.getRefreshToken(member);
 
                 log.info("refreshToken 만료 여부 확인 중..");
-                if (jwtAuthenticationToken.validTokenExpired(refreshToken)) {  // accessToken이 Null이 아니면서 만료되고, refreshToken은 살아 있을 때
+                if (jwtToken.validTokenExpired(refreshToken)) {  // accessToken이 Null이 아니면서 만료되고, refreshToken은 살아 있을 때
 
                     log.info("refreshToken이 만료되지 않았습니다.");
                     log.info("토큰 재발급 중..");
-                    String reAccessToken = jwtAuthenticationToken.createAccessToken(member.getEmail());
-                    String reRefreshToken = jwtAuthenticationToken.createRefreshToken(member.getEmail());
+                    String reAccessToken = jwtToken.createAccessToken(member.getEmail());
+                    String reRefreshToken = jwtToken.createRefreshToken(member.getEmail());
 
                     TokenEditor tokenEditor = TokenEditor.builder()
                             .accessToken(reAccessToken)
@@ -64,7 +63,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                             .build();
 
                     tokenService.reIssueToken(tokenEditor, member);
-                    authentication = jwtAuthenticationToken.getAuthentication(reAccessToken);
+                    authentication = jwtToken.getAuthentication(reAccessToken);
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 } else {
                     log.info("refreshToken 만료. 재 로그인 부탁드립니다.");
