c0480864dde54daf0bb7e06477075c55106d024f
Park-Kwanwoo <pgw4712@icloud.com>
2022-09-25 19:05:10 +0900

    클래스명 변경

--- src/main/java/org/project/weathercommunity/response/post/PostListResponse.java
+++ src/main/java/org/project/weathercommunity/response/post/PostListResponse.java
@@ -8,25 +8,28 @@ import org.project.weathercommunity.domain.post.Post;
  * 서비스 정책에 맞는 응답 클래스
  */
 @Getter
-public class PostResponse {
+public class PostListResponse {
 
     private final Long id;
     private final String title;
     private final String content;
-    private final Long memberId;
+    private final String createdTime;
+    private final String memberName;
 
     // 생성자 오버로딩
-    public PostResponse(Post post) {
+    public PostListResponse(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
-        this.memberId = post.getMember().getId();
+        this.createdTime = post.getCreatedDate().substring(5, 16);
+        this.memberName = post.getMember().getName();
     }
     @Builder
-    public PostResponse(Long id, String title, String content, Long memberId) {
+    public PostListResponse(Long id, String title, String content, String createdTime, String memberName) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.memberId = memberId;
+        this.createdTime = createdTime;
+        this.memberName = memberName;
     }
 }
