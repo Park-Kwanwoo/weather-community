d3930f7520895fc4b15f97f06e9e0c4509381c08
Park-Kwanwoo <pgw4712@icloud.com>
2022-10-11 16:31:33 +0900

    회원 응답 필드 수정

--- src/main/java/org/project/weathercommunity/response/member/MemberMypageResponse.java
+++ src/main/java/org/project/weathercommunity/response/member/MemberMypageResponse.java
@@ -7,27 +7,21 @@ import org.project.weathercommunity.domain.member.Member;
 @Getter
 public class MemberMypageResponse {
 
-    private final Long id;
     private final String email;
 
-    private final String password;
-    private final String name;
+    private final String nickname;
     private final String phone;
 
     public MemberMypageResponse(Member member) {
-        this.id = member.getId();
         this.email = member.getEmail();
-        this.password = member.getPassword();
-        this.name = member.getName();
+        this.nickname = member.getNickname();
         this.phone = member.getPhone();
     }
 
     @Builder
-    public MemberMypageResponse(Long id, String email, String password, String name, String phone) {
-        this.id = id;
+    public MemberMypageResponse(String email, String nickname, String phone) {
         this.email = email;
-        this.password = password;
-        this.name = name;
+        this.nickname = nickname;
         this.phone = phone;
     }
 }
