c7d2df00c46ca84ac27c27db57cbc33294caafc4
Park-Kwanwoo <pgw4712@icloud.com>
2022-10-11 16:31:47 +0900

    회원 닉네임 중복 에러 클래스

---
+++ src/main/java/org/project/weathercommunity/exception/MemberEmailDuplicationException.java
@@ -0,0 +1,24 @@
+package org.project.weathercommunity.exception;
+
+public class MemberEmailDuplicationException extends WeatherCommunityException {
+
+    private static final String MESSAGE = "사용할 수 없는 이메일입니다.";
+
+    public MemberEmailDuplicationException() {
+        super(MESSAGE);
+    }
+
+    public MemberEmailDuplicationException(Throwable cause) {
+        super(MESSAGE, cause);
+    }
+
+    public MemberEmailDuplicationException(String fieldName, String message) {
+        super(MESSAGE);
+        addValidation(fieldName, message);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
+}
