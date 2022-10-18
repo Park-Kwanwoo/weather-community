e8327ae98d452c0f759339751a0ec562f5c60ec0
Park-Kwanwoo <pgw4712@icloud.com>
2022-10-18 18:50:30 +0900

    회원 Rest Document 수정

--- src/test/java/org/project/weathercommunity/controller/member/MemberControllerDocTest.java
+++ src/test/java/org/project/weathercommunity/controller/member/MemberControllerDocTest.java
@@ -1,11 +1,15 @@
 package org.project.weathercommunity.controller.member;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.project.weathercommunity.config.security.token.JwtTokenProvider;
 import org.project.weathercommunity.domain.member.Member;
+import org.project.weathercommunity.domain.token.Token;
 import org.project.weathercommunity.repository.member.MemberRepository;
+import org.project.weathercommunity.repository.token.TokenRepository;
 import org.project.weathercommunity.request.member.MemberCreate;
 import org.project.weathercommunity.request.member.MemberEdit;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +17,11 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -31,29 +38,64 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(RestDocumentationExtension.class)
 public class MemberControllerDocTest {
 
+    @Autowired
+    MemberRepository memberRepository;
 
     @Autowired
-    private MockMvc mockMvc;
+    TokenRepository tokenRepository;
 
     @Autowired
-    private MemberRepository memberRepository;
+    ObjectMapper objectMapper;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    MockMvc mockMvc;
 
-    @Test
-    @DisplayName("회원 등록")
-    void 회원_등록_Doc() throws Exception {
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
 
-        // given
-        MemberCreate request = MemberCreate.builder()
-                .email("test@case.com")
-                .name("테스터")
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    private MemberCreate memberCreate() {
+        return MemberCreate.builder() .email("test@case.com")
+                .nickname("테스터")
                 .phone("010-2314-1232")
                 .password("tester12#")
                 .build();
+    }
 
-        String json = objectMapper.writeValueAsString(request);
+    private Member member(MemberCreate memberCreate) {
+        return Member.builder()
+                .email(memberCreate.getEmail())
+                .nickname(memberCreate.getNickname())
+                .phone(memberCreate.getPhone())
+                .password(passwordEncoder.encode(memberCreate().getPassword()))
+                .build();
+    }
+
+
+    private Token createToken(Member member) {
+        return Token.builder()
+                .accessToken(jwtTokenProvider.createAccessToken(member.getEmail()))
+                .refreshToken(jwtTokenProvider.createRefreshToken(member.getEmail()))
+                .member(member)
+                .build();
+    }
+
+    @BeforeEach
+    void clean() {
+        memberRepository.deleteAll();
+        tokenRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("회원 등록 DOCUMENT")
+    void MEMBER_JOIN_DOCUMENT() throws Exception {
+
+        // given
+        MemberCreate memberCreate = memberCreate();
+
+        String json = objectMapper.writeValueAsString(memberCreate);
 
         // expected
         mockMvc.perform(post("/members/join")
@@ -64,9 +106,9 @@ public class MemberControllerDocTest {
                 .andExpect(status().isOk())
                 .andDo(document("member-join",
                         requestFields(
-                                fieldWithPath("email").description("제목"),
+                                fieldWithPath("email").description("이메일"),
                                 fieldWithPath("password").description("비밀번호"),
-                                fieldWithPath("name").description("이름"),
+                                fieldWithPath("nickname").description("닉네임"),
                                 fieldWithPath("phone").description("전화번호"),
                                 fieldWithPath("role").description("회원 등급")
 
@@ -75,29 +117,28 @@ public class MemberControllerDocTest {
     }
 
     @Test
-    @DisplayName("회원 수정_doc")
-    void 회원_수정() throws Exception {
+    @DisplayName("회원 수정 DOCUMENT")
+    void MEMBER_EDIT_DOCUMENT() throws Exception {
 
         // given
-        Member member = Member.builder()
-                .email("test@case.com")
-                .name("테스터")
-                .phone("010-2314-1232")
-                .password("tester12#")
-                .build();
+        MemberCreate memberCreate = memberCreate();
+        Member member = member(memberCreate);
 
         memberRepository.save(member);
 
+        Token token = createToken(member);
+
         MemberEdit memberEdit = MemberEdit.builder()
-                .name("테스터")
-                .password("tester34#")
-                .phone("010-1111-1111")
+                .nickname("닉네임")
                 .build();
+
+        Token savedToken = tokenRepository.save(token);
 
         String jsonValue = objectMapper.writeValueAsString(memberEdit);
 
         // expected
         mockMvc.perform(patch("/members/{memberId}", member.getId())
+                        .header(AUTHORIZATION, savedToken.getAccessToken())
                         .contentType(APPLICATION_JSON)
                         .accept(APPLICATION_JSON)
                         .content(jsonValue)
@@ -110,30 +151,28 @@ public class MemberControllerDocTest {
                                         parameterWithName("memberId").description("회원 ID")
                                 ),
                                 requestFields(
-                                        fieldWithPath("password").description("비밀번호"),
-                                        fieldWithPath("name").description("이름"),
-                                        fieldWithPath("phone").description("전화번호")
+                                        fieldWithPath("nickname").description("닉네임")
                                 )
                         )
                 );
     }
 
     @Test
-    @DisplayName("회원 삭제_doc")
-    void 회원_삭제() throws Exception {
+    @DisplayName("회원 삭제 DOCUMENT")
+    void MEMBER_DELETE_DOCUMENT() throws Exception {
 
         // given
-        Member member = Member.builder()
-                .email("test@case.com")
-                .name("테스터")
-                .phone("010-2314-1232")
-                .password("tester12#")
-                .build();
+        MemberCreate memberCreate = memberCreate();
+        Member member = member(memberCreate);
 
         memberRepository.save(member);
 
+        Token token = createToken(member);
+        Token savedToken = tokenRepository.save(token);
+
         // expected
         mockMvc.perform(delete("/members/{memberId}", member.getId())
+                        .header(AUTHORIZATION, savedToken.getAccessToken())
                         .contentType(APPLICATION_JSON)
                         .accept(APPLICATION_JSON)
                 )
