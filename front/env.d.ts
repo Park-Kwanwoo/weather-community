365e22da20056618b39e81202dd25e9939292bd3
Park-Kwanwoo <pgw4712@icloud.com>
2022-10-24 16:04:32 +0900

    백 프론트 폴더 분리

--- front/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-    VITE_MAP_KEY: string
-    VITE_LOCAL_KEY: string
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
--- front/src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <el-footer align="center">
-    copyright 2022. Dev Park rights reserved.
-  </el-footer>
-</template>
