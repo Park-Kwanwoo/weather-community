6d493865dbfe968a6eaa517e080ed5517f554dc3
Park-Kwanwoo <pgw4712@icloud.com>
2022-10-29 21:44:31 +0900

    POST에서 GET방식으로 변경

--- src/main/java/org/project/weathercommunity/controller/weather/WeatherController.java
+++ src/main/java/org/project/weathercommunity/controller/weather/WeatherController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.project.weathercommunity.request.weather.WeatherRequest;
 import org.project.weathercommunity.service.weather.WeatherService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -16,8 +18,8 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
-    @PostMapping("/weather/forecast")
-    public Mono<Object> ultraShortForecast(@RequestBody @Valid WeatherRequest weatherRequest) {
+    @GetMapping("/weather/forecast")
+    public Mono<Object> ultraShortForecast(@ModelAttribute @Valid WeatherRequest weatherRequest) {
         return weatherService.ultraShortForecast(weatherRequest);
     }
 }
