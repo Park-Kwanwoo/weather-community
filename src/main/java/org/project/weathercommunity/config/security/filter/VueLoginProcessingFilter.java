082a846f7a099f9bc409c983d17800bc7919db08
Park-Kwanwoo <pgw4712@icloud.com>
2022-11-22 20:16:00 +0900

    홈페이지 로그인, Kakao 로그인 분리

--- src/main/java/org/project/weathercommunity/config/security/filter/VueLoginProcessingFilter.java
+++ src/main/java/org/project/weathercommunity/config/security/filter/VueLoginProcessingFilter.java
@@ -40,12 +40,23 @@ public class VueLoginProcessingFilter extends AbstractAuthenticationProcessingFi
         }
 
         MemberLogin memberLogin = objectMapper.readValue(request.getReader(), MemberLogin.class);
-        if (!StringUtils.hasText(memberLogin.getPassword()) || !StringUtils.hasText(memberLogin.getEmail())) {
-            throw new LoginValueEmptyException("value","이메일과 비밀번호를 제대로 입력해주세요.");
+        VueAuthenticationToken token;
+
+        // 카카오 로그인의 경우
+        if (memberLogin.getAuth().equals("kakao")) {
+
+            if (!StringUtils.hasText(memberLogin.getEmail())) {
+                throw new LoginValueEmptyException("email", "등록되지 않은 이메일입니다.");
+            }
+
+            token = new VueAuthenticationToken(memberLogin.getEmail(), null);
+        } else {
+            if (!StringUtils.hasText(memberLogin.getPassword()) || !StringUtils.hasText(memberLogin.getEmail())) {
+                throw new LoginValueEmptyException("value","이메일과 비밀번호를 제대로 입력해주세요.");
+            }
+            token = new VueAuthenticationToken(memberLogin.getEmail(), memberLogin.getPassword());
+
         }
-
-        VueAuthenticationToken token = new VueAuthenticationToken(memberLogin.getEmail(), memberLogin.getPassword());
-
         return getAuthenticationManager().authenticate(token);
     }
 
