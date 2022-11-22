6095d8027b2d98ebb1574d55f9781ce936e25646
Park-Kwanwoo <pgw4712@icloud.com>
2022-11-22 21:27:57 +0900

    비밀번호 @Pattern 설정

--- src/main/java/org/project/weathercommunity/request/member/MemberCreate.java
+++ src/main/java/org/project/weathercommunity/request/member/MemberCreate.java
@@ -17,9 +17,8 @@ public class MemberCreate {
     @Email(message = "제대로된 이메일 형식으로 입력해주세요.", regexp = "^[\\w\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
     private String email;
 
-//    @NotBlank(message = "비밀번호를 입력해주세요.")
-//    @Pattern(regexp = "^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
-//             message = "숫자, 문자, 특수문자 포함 8~15자리 이내로 입력해주세요.")
+    @Pattern(regexp = "^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$",
+             message = "숫자, 문자, 특수문자 포함 8~15자리 이내로 입력해주세요.")
     private String password;
 
     @NotBlank(message = "이름을 입력해주세요.")
